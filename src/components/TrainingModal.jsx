37c022b5338dea4a622ed677c61e04abccda5d1d
H.Vasev <hristovasev999@gmail.com>
2024-08-02 14:20:11 +0100

    Implement custom exercise duration

--- src/components/TrainingModal.jsx
+++ src/components/TrainingModal.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 
 export default function TrainingModal({ videos }) {
   const [video, setVideo] = useState(videos);
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(0);
 
   let currentVideo = video.length > 0 ? video[0] : "";
 
+  let remainder = parseInt(currentVideo.description);
+
   useEffect(() => {
-    if (currentVideo !== "") {
+    if (currentVideo !== "" && currentVideo.duration === "Seconds") {
+      setCount(remainder);
+
       const interval = setInterval(() => {
         setCount((prev) => {
           if (prev === 1) {
@@ -17,13 +21,13 @@ export default function TrainingModal({ videos }) {
               return currState;
             });
 
-            return 5;
+            return remainder;
           } else {
             return prev - 1;
           }
         });
       }, 1000);
-
+      
       return () => clearInterval(interval);
     }
   }, [video, currentVideo]);
@@ -35,16 +39,25 @@ export default function TrainingModal({ videos }) {
       return currState;
     });
 
-    setCount(5)
+    setCount(5);
   }
 
   return (
     <>
       <h1>Here</h1>
-      <p>Time left: {count}</p>
-      {currentVideo != "" ? <video key={currentVideo.url} width="600" loop autoPlay>
-        <source src={currentVideo.url} type="video/mp4" />
-      </video> : <p>Congratulation you already complete the exercises!</p>}
+      {currentVideo !== "" && currentVideo.duration === "Seconds" && (
+        <p>Time left: {count}</p>
+      )}
+      {currentVideo !== "" && currentVideo.duration !== "Seconds" && (
+        <p>Times: {currentVideo.description}</p>
+      )}
+      {currentVideo !== "" ? (
+        <video key={currentVideo.url} width="600" loop autoPlay>
+          <source src={currentVideo.url} type="video/mp4" />
+        </video>
+      ) : (
+        <p>Congratulation you already complete the exercises!</p>
+      )}
       <button onClick={videoHandler}>Next</button>
     </>
   );
