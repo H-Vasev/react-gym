ed0fca1b3ad07a30f324727f85c328d68e29034a
H.Vasev <hristovasev999@gmail.com>
2024-09-20 17:12:26 +0100

    Add register Page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,23 +1,82 @@
+import { useState } from "react";
 import classes from "./LogIn.module.css";
+import { useRef } from "react";
 
 export default function LogIn() {
+  const [signUp, setSignUp] = useState(false);
+  const [messages, setMessages] = useState({
+    isError: false,
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  function handelSignUp() {
+    setSignUp((state) => !state);
+  }
+
+  function handleRegister(event) {
+    event.preventDefault();
+
+    const formData = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      confirmPassword: signUp ? confirmPasswordRef.current.value : null,
+    };
+
+    if (
+      formData.confirmPassword != null &&
+      formData.password != formData.confirmPassword
+    ) {
+      setMessages((prevState) => {
+        return {
+          ...prevState,
+          isError: true,
+          confirmPassword: "Does not match the password!",
+        };
+      });
+    }
+
+    console.log(formData);
+
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+    confirmPasswordRef.current.value = "";
+  }
+
   return (
     <>
       <div className={classes.container}>
         <div>
-          <h1>LogIn</h1>
-          <div className={classes["form-container"]}>
+          <h1>{signUp ? "Register" : "LogIn"}</h1>
+          <form className={classes["form-container"]}>
             <label>Email</label>
-            <input type="email" />
-
+            <input type="email" ref={emailRef} required />
             <label>Password</label>
-            <input type="text" />
-
+            <input type="password" ref={passwordRef} required />
+            {signUp && <label>Confirm Password</label>}
+            {signUp && (
+              <input type="password" ref={confirmPasswordRef} required />            
+            )}
+            {messages.isError && <p>{messages.confirmPassword}</p>}
             <div className={classes["button-container"]}>
-              <button>LogIn</button>
-              <button>Register</button>
+              {!signUp && <button type="button">LogIn</button>}
+              {!signUp && <span>or</span>}
+              {!signUp ? (
+                <button type="button" onClick={handelSignUp}>
+                  Sign Up
+                </button>
+              ) : (
+                <button type="button" onClick={handleRegister}>
+                  Register
+                </button>
+              )}
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
